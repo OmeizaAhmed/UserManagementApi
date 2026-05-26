cad5b9246326d1c17110b58da0a15ec123af0347
Ahmed Omeiza <omeizaahmed81@gmail.com>
2026-05-26 13:57:55 +0100

    another commit

--- obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/UserManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a02cbb90d4d4f9aeabf889e2afe3504c0c2714")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
